7c774603113623c1949fd1bfaa839ed631bebc60
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-18 10:35:50 -0400

    added and imported ui router to angular app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+function config () {
+
+}
+config.$inject = [];
+export { config }
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,21 @@
 import angular from 'angular';
+import 'angular-ui-router';
 
 // Import Our Controllers
 import { AddController } from './controllers/add.controller';
 import { ListController } from './controllers/list.controller';
 import { SingleController } from './controllers/single.controller';
 
-// Register our module
+// Import our Configuration
+import { config } from './config'
+
+// Start Angular
 angular
-  // Setter for a module
-  .module('app', [])
-  // Register our controller
+  // Setter for a module(register)
+  .module('app', ['ui.router'])
+  // Register our configuration
+    .config(config) // only have one so you don't have to give it a name like the controllers below
+  // Register our controllers
   .controller('AddController', AddController)
   .controller('ListController', ListController)
   .controller('SingleController', SingleController)
