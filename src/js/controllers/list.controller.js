b31434410bbebee430caa98fe5bb52ee74be34a4
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-18 12:08:40 -0400

    added a function for list controller and template for list html

--- src/js/controllers/list.controller.js
+++ src/js/controllers/list.controller.js
@@ -1,7 +1,16 @@
 // Dependency Injection Function
-function ListController () {
+function ListController ($scope, $http, URL) {
+
+  init();
+
+  function init() {
+    $http.get(URL).then( (res) => {
+      console.log(res);
+      $scope.allFoods = res.data;
+    });
+  }
 
 }
 
-ListController.$inject = [];
+ListController.$inject = ['$scope', '$http', 'URL'];
 export { ListController }
